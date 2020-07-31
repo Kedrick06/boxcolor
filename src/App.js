071d4cccedec8937b34506b830fc2a619b1818bb
Kedrick06 <bfloblk@gmail.com>
2020-07-31 15:52:11 -0500

    boxcolor

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Box from './Components/Box';
+
+function App() {
+
+  return (
+    <div className="App">
+
+    <h1>Check out this app!</h1>
+      
+    <Box />
+   </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Box.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+export default class Box extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = { color:"blue" };
+    this.changeColor = this.changeColor.bind(this);
+  }
+
+  changeColor(){
+  let red = Math.floor(Math.random()*255)
+  let green = Math.floor(Math.random()*255)
+  let blue =  Math.floor(Math.random()*255)
+  console.log(this.state.color)
+  this.setState({color:`rgb(${red},${green},${blue})`})
+  }
+
+  render(){
+    return(
+      <body className="div" style={{backgroundColor: this.state.color}}>
+      <h1>Changed color</h1>
+      <button className="button" onClick={this.changeColor}>CHANGE MY COLOR</button>
+      </body>
+    )
+  }
+}
+
